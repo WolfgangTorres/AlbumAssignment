7dbc8eba0b7a72987eb3d7b106a94808eed7cb0c
WolfgangTorres <andrestorresb95@gmail.com>
2024-01-28 18:23:08 -0600

    dark and light themes

---
+++ src/utils/styles/darkTheme.ts
@@ -0,0 +1,16 @@
+export const DarkTheme = {
+  dark: true,
+  colors: {
+    primary: 'rgb(10, 132, 255)', // Use for interactive elements
+    background: 'rgb(28, 28, 30)', // Dark background
+    card: 'rgb(38, 38, 40)', // Background for card-like elements
+    text: 'rgb(229, 229, 231)', // Main text color
+    border: 'rgb(58, 58, 60)', // Borders or lines that separate elements
+    headerTitle: 'rgb(255, 255, 255)', // Title in headers
+    title: 'rgb(235, 235, 245)', // Section titles
+    subtitle: 'rgb(174, 174, 178)', // Subtitles or secondary text
+    buttonBackground: 'rgb(255, 69, 58)', // Red background for buttons
+    buttonText: 'rgb(255, 255, 255)', // Text color for buttons
+    notification: 'rgb(255, 59, 48)',
+  },
+}
---
+++ src/utils/styles/lightTheme.ts
@@ -0,0 +1,16 @@
+export const LightTheme = {
+  dark: false,
+  colors: {
+    primary: 'rgb(0, 122, 255)', // Use for interactive elements
+    background: 'rgb(242, 242, 242)', // Light gray background
+    card: 'rgb(255, 255, 255)', // Background for card-like elements
+    text: 'rgb(28, 28, 30)', // Main text color
+    border: 'rgb(216, 216, 216)', // Borders or lines that separate elements
+    headerTitle: 'rgb(0, 0, 0)', // Title in headers
+    title: 'rgb(23, 23, 23)', // Section titles
+    subtitle: 'rgb(99, 99, 102)', // Subtitles or secondary text
+    buttonBackground: 'rgb(255, 59, 48)', // Red background for buttons
+    buttonText: 'rgb(255, 255, 255)', // Text color for buttons
+    notification: 'rgb(255, 69, 58)',
+  },
+}
