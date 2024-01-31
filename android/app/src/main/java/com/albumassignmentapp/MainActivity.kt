dda68039417b2b196aeef374b4ceeaa9b60c5006
WolfgangTorres <andrestorresb95@gmail.com>
2024-01-30 23:50:03 -0600

    app icons and splash screen

--- android/app/src/main/java/com/albumassignmentapp/MainActivity.kt
+++ android/app/src/main/java/com/albumassignmentapp/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.albumassignmentapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.widget.LinearLayout
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
+
+  private lateinit var splashScreen: LinearLayout
+  private val splashTimeOut: Long = 5000 // 3 sec
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
@@ -16,6 +22,22 @@ class MainActivity : ReactActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(null)
+
+    // Inflate the splash screen layout
+    splashScreen = layoutInflater.inflate(R.layout.splash_screen, null) as LinearLayout
+    addContentView(splashScreen, LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT))
+
+    Handler().postDelayed({
+      // Start your app main activity
+      this.hideSplashScreen()
+    }, splashTimeOut)
+  }
+
+  fun hideSplashScreen() {
+    runOnUiThread {
+      // Remove the splash screen from view
+      splashScreen.visibility = LinearLayout.GONE
+    }
   }
 
   /**
